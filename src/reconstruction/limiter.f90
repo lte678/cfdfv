dbcc710b7522133ba11c91d4ec49cd7dcb582574
Leon Teichroeb <bigleon00@gmail.com>
2023-12-09 18:02:32 +0100

    Solution 3.2 Limiter

--- src/reconstruction/limiter.f90
+++ src/reconstruction/limiter.f90
@@ -86,10 +86,42 @@ INTEGER                 :: iVar
 TYPE(tSide), POINTER    :: aSide
 !===================================================================================================================================
 
-! this routine computes the limited gradients aElem%u_x and aElem%u_y
+  ! this routine computes the limited gradients aElem%u_x and aElem%u_y
+  ! Get the min and max values of the neighbouring cells
+  uMin = aElem%pvar
+  uMax = aElem%pvar
+  aSide => aElem%firstSide
+  DO WHILE(ASSOCIATED(aSide))
+    uMax(:) = MAX(uMax, aSide%connection%Elem%pvar)
+    uMin(:) = MIN(uMin, aSide%connection%Elem%pvar)
+    aSide => aSide%nextElemSide
+  END DO
 
-        ! Insert your Code here
-        
+  MaxDiff(:) = uMax - aElem%pvar
+  MinDiff(:) = uMin - aElem%pvar
+
+  phi(:) = 1.0
+  aSide => aElem%firstSide
+  DO WHILE(ASSOCIATED(aSide))
+    uDiff = aElem%u_x * aSide%GP(X_DIR) + aElem%u_y * aSide%GP(Y_DIR) 
+    phiLoc(:) = 0
+    DO iVar = 1, NVAR
+      IF(uDiff(iVar) > 0.0) THEN
+        phiLoc(iVar) = MIN(1.0, MaxDiff(iVar)/uDiff(iVar))
+      ELSEIF(uDiff(iVar) < 0.0) THEN
+        phiLoc(iVar) = MIN(1.0, MinDiff(iVar)/uDiff(iVar))
+      END IF
+    END DO
+
+    ! Take the minimum scaler
+    phi(:) = MIN(phi, phiLoc)
+    aSide => aSide%nextElemSide
+  END DO
+
+  ! Apply limit
+  aElem%u_x(:) = aElem%u_x * phi
+  aElem%u_y(:) = aElem%u_y * phi
+
 END SUBROUTINE Limiter_BarthJespersen
 
 
@@ -120,10 +152,54 @@ TYPE(tSide), POINTER    :: aSide
 
 ! this routine computes the limited gradients aElem%u_x and aElem%u_y
 
-    ! variables with reqired data:
-    ! epsilon^2: aElem%venk_epsilon_sq
+  ! variables with reqired data:
+  ! epsilon^2: aElem%venk_epsilon_sq
 
-        ! Insert your Code here
+  ! Get the min and max values of the neighbouring cells
+  uMin = aElem%pvar
+  uMax = aElem%pvar
+  aSide => aElem%firstSide
+  DO WHILE(ASSOCIATED(aSide))
+    uMax(:) = MAX(uMax, aSide%connection%Elem%pvar)
+    uMin(:) = MIN(uMin, aSide%connection%Elem%pvar)
+    aSide => aSide%nextElemSide
+  END DO
+
+  MaxDiff(:) = uMax - aElem%pvar
+  MinDiff(:) = uMin - aElem%pvar
+  MaxDiff_sq(:) = MaxDiff * MaxDiff
+  MinDiff_sq(:) = MinDiff * MinDiff
+
+  phi(:) = 1.0
+  aSide => aElem%firstSide
+  DO WHILE(ASSOCIATED(aSide))
+    uDiff(:) = aElem%u_x * aSide%GP(X_DIR) + aElem%u_y * aSide%GP(Y_DIR)
+    uDiff_sq(:) = uDiff * uDiff
+    phiLoc(:) = 0
+    DO iVar = 1, NVAR
+      IF(uDiff(iVar) > 0.0) THEN
+        phiLoc(iVar) = 1. / uDiff(iVar) * &
+          (((MaxDiff_sq(iVar) + aElem%venk_epsilon_sq) * uDiff(iVar) + &
+          2. * uDiff_sq(iVar) * MaxDiff(iVar))                       / &
+          (MaxDiff_sq(iVar) + 2. * uDiff_sq(iVar) + uDiff(iVar)      * &
+          MaxDiff(iVar) + aElem%venk_epsilon_sq))
+      ELSEIF(uDiff(iVar) < 0.0) THEN
+        phiLoc(iVar) = 1. / uDiff(iVar) * &
+          (((minDiff_sq(iVar) + aElem%venk_epsilon_sq) * uDiff(iVar) + &
+          2. * uDiff_sq(iVar) * minDiff(iVar))                       / &
+          (minDiff_sq(iVar) + 2. * uDiff_sq(iVar) + uDiff(iVar)      * &
+          minDiff(iVar) + aElem%venk_epsilon_sq))
+      END IF
+    END DO
+
+    ! Take the minimum scaler
+    phi(:) = MIN(phi, phiLoc)
+    aSide => aSide%nextElemSide
+  END DO
+
+  ! Apply limit
+  aElem%u_x(:) = aElem%u_x * phi
+  aElem%u_y(:) = aElem%u_y * phi
 !-----------------------------------------------------------------------------------------------------------------------------------
 END SUBROUTINE Limiter_Venkatakrishnan
 
